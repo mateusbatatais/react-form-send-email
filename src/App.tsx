8c463d5a81ac7d5ca09842addc239522d9824425
Mateus Severiano <mateusbatatais@gmail.com>
2022-03-28 06:08:13 -0300

    start base project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Container>teste</Container>
+      </header>
+    </div>
+  );
+}
+
+export default App;
