8bb8a689c979e41c05a53defcdd53b61f9be9c96
Mateus Severiano <mateusbatatais@gmail.com>
2022-03-31 12:58:48 -0300

    estrutura lista de paginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.scss";
 
+import TopMenu from "./components/atoms/molecules/TopMenu";
+
 import PersonaData from "./pages/PersonalData";
+import ListItens from "./pages/ListItens";
 
 function App() {
   return (
     <Router>
+      <TopMenu />
       <div className="App">
         <Routes>
           <Route path="/" element={<PersonaData />} />
+          <Route path="/listagem" element={<ListItens />} />
         </Routes>
       </div>
     </Router>
---
+++ src/pages/ListItens/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+function ListItens() {
+  return <Container>Lista</Container>;
+}
+
+export default ListItens;
